14e8ebbc88e2a7ac6b0cffd05985855d91a83b55
Pawan Kumar Thakur <Kumarthakurpawan788@gmail.com>
2024-11-25 20:31:08 +0530

    update

--- test.js
+++ test.js
@@ -1,59 +1,57 @@
-
 /*with jwt token  */
 
-const express= require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
-const secretKey = "secrekey"
+const secretKey = "secrekey";
 
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
   res.json({
-    message:'testing  api '
-  })
-})
+    message: "testing  api ",
+  });
+});
 
+app.post("/login", (req, res) => {
+  const user = {
+    id: 1,
+    name: "Pawan",
+    email: "test@gmail.com",
+  };
+  jwt.sign({ user }, secretKey, { expiresIn: "300s" }, (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
 
-app.post('/login',(req,res)=>{
-    const user = {
-        id:1,
-        name:'Pawan',
-        email:'test@gmail.com'
+app.post("/profile", verifyToken, (req, res) => {
+  jwt.verify(req.token, secretKey, (err, authData) => {
+    if (err) {
+      console.error("JWT Error:", err);
+      res.send({ result: "Invalid Token" });
+    } else {
+      res.json({
+        message: "profile accessed ",
+        authData,
+      });
     }
-    jwt.sign({user},secretKey,{ expiresIn:'300s'},(err,token)=>{
-        res.json({
-            token
-        })
-    })
-})
+  });
+});
 
-app.post('/profile',verifyToken,(req,res)=>{
-    jwt.verify(req.token,secretKey,(err,authData)=>{
-        if(err){
-            res.send({result:"Invalid Token"})
-        }else{
-            res.json({
-                message:"profile ",
-                authData
-            })
-        }
-    })
-})
-
-function verifyToken(req,res,next){
-
-    const bearer =req.headers['Authorization'];
-    if(typeof bearer !== 'undefined'){
-        const bearerhead = bearer.split(' ');
-        const token = bearerhead[1];
-        req.token = token;
-    }else{
-        res.send({
-            result:'Token is not valid'
-        })
-    }
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const token = bearer[1];
+    req.token = token;
+    next(); // Call next to continue
+  } else {
+    res.status(403).send({
+      result: "Token is not valid",
+    });
+  }
 }
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
   console.log("App is running in port http://localhost:5000");
-  
-})
+});
